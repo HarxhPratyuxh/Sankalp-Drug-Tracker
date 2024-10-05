f74e78d16a14cb60b8577ddacfbb744568f49a91
harxhpratyuxh <harxhpratyuxh@gmail.com>
2024-10-05 15:48:58 +0530

    nice

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import requests;
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/track')
+def track():
     return render_template('track.html')
 @app.route('/buri')
 def buri():
     return render_template('buri.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/check_blacklist', methods=['POST'])
